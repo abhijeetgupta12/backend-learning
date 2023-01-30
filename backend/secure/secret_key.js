3ae2bb2a890445b3bca439bb40df54458f6d060f
Abhijeet Kumar Gupta <42419180+abhijeetgupta12@users.noreply.github.com>
2023-01-30 17:37:50 +0530

    Delete secret_key.js
    Confidential file

--- backend/secure/secret_key.js
+++
@@ -1,6 +0,0 @@
-const SECRET_KEY = 'jsdckjsdbckkjsckjbksc';
-const MAIL_PASS = "pvioaaodwcwlynxx";
-const MAIL = "abhijeet.gpta12@gmail.com"
-const DB_LINK = "mongodb+srv://admin:9DcRgaJANlE81zvl@cluster0.bhvdu.mongodb.net/?retryWrites=true&w=majority"
-
-module.exports={SECRET_KEY,MAIL,MAIL_PASS,DB_LINK};
